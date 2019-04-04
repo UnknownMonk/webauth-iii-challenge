2609564ed80f59262e4d25ab035210bd09fe7293
UnknownMonk <smodcartoon@gmail.com>
2019-04-04 17:07:34 -0400

    setup react app and routes

---
+++ login-app/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
+import './App.css';
+import Login from './login/Login';
+import Users from './users/Users';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <NavLink to="/">Home</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/login">Login</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/users">Users</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/signup">Singn up</NavLink>
+          <button onClick={this.logout}>Logout</button>
+        </header>
+        <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/users" component={Users} />
+        </main>
+      </>
+    );
+  }
+
+  logout = () => {
+    localStorage.removeItem('token');
+  };
+}
+
+function Home(props) {
+  return <h1>Home Component</h1>;
+}
+
+export default App;
